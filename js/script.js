93674675730b926e4b7a07fb5eb745c80a17d85f
seanniekins <seanaustinj@gmail.com>
2020-09-09 20:29:54 +0800

    Revamped number and clear functions done

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ var solutionElement = document.getElementById("solution-text-value");
 var answerElement = document.getElementById("answer-text-value");
 var solution;
 var answer;
+var numbersObj = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
 
 Array.from(document.getElementsByTagName("button")).forEach(function(element) {
     element.addEventListener("click", performChanges);
@@ -11,21 +12,22 @@ function performChanges() {
     solution = Array.from(solutionElement.textContent); // convert to array for line value manipulation
     answer = Array.from(answerElement.textContent);
 
-    console.log(solution, answer);
-
     functionController();
 
     solutionElement.textContent = solution.join(""); // return back to string type
     answerElement.textContent = answer.join("");
 }
 
-function functionController() {
+function functionController() { // classify by class
     switch(event.target.className) {
         case "number":
             writeNumber();
             break;
         case "operation":
             writeOperation();
+            break;
+        case "function":
+            executeFunction();
             break;
         case "zero":
             writeZero();
@@ -37,12 +39,18 @@ function functionController() {
             console.log("switch default");
             break;
     }
-
-    console.log(event.target.className);
 }
 
-function writeNumber() {
+function executeFunction() {
+    if(event.target.id == "clear") {
+        solution.pop();
+    }
+}
 
+function writeNumber() { // classify by id
+    if(numbersObj.hasOwnProperty(event.target.id)) {
+        solution.push(numbersObj[event.target.id]);
+    }
 }
 
 function writeOperator() {
