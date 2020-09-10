9db0dba5ffb22000a75b6caeba41cb79d79f07d5
seanniekins <seanaustinj@gmail.com>
2020-09-10 22:17:14 +0800

    Finished tasks 1 and 2, will start on writing parser

--- js/script.js
+++ js/script.js
@@ -1,36 +1,43 @@
-var solutionElement = document.getElementById("solution-text-value");
-var answerElement = document.getElementById("answer-text-value");
+/*
+    TODO:
+    OK  1. Allow input of numbers, operations, zero, and period, then display on screen
+    OK  2. Create clear function deleting one character, and clear entry function deleting everything on screen
+        3. Solve the input on the screen by writing a parser function (instead of using eval()*) and display it
+        4. Eliminate successive operations
+        5. Eliminate successive decimals
+        6. Eliminate successive zeroes
+        7. Implement plus-minus function
+        8. Go crazy on additional features
+
+        * using eval() has alleged potential security risks -- plus writing a parser is a good exercise :)
+*/
+
 var solution;
 var answer;
-var solutionLastChar;
-var solution2ndLastChar;
-var numbersObj = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
-var plusSign = String.fromCharCode(43); // plus
-var minusSign = String.fromCharCode(45); // hyphen
-var timesSign = String.fromCharCode(215); // times
-var divideSign = String.fromCharCode(247); // divide
-var opChars = {"plus": String.fromCharCode(43), "minus": String.fromCharCode(45), "times": String.fromCharCode(215), "divide": String.fromCharCode(247)}; // key names matched with element ids for easy reference calls
-var opArr = [opChars["plus"], opChars["minus"], opChars["times"], opChars["divide"]]; // used for checking presence of operation
+var numbersArr = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9};
+var opArr = {"plus": "+", "minus": "-", "times": "×", "divide": "÷"};
 
-function opExists(myChar) { return opArr.includes(myChar) ? true : false; } // wrote because opArr.includes() is confusing
+Array.from(document.getElementsByClassName("number")).forEach(function(element) {
+    element.addEventListener("click", performUpdate);
+})
 
-Array.from(document.getElementsByTagName("button")).forEach(function(element) {
-    element.addEventListener("click", performChanges);
-}); 
+Array.from(document.getElementsByClassName("operation")).forEach(function(element) {
+    element.addEventListener("click", performUpdate);
+})
 
-function performChanges() {
-    solution = Array.from(solutionElement.textContent); // convert to array for line value manipulation
-    answer = Array.from(answerElement.textContent);
-    solutionLastChar = solution[solution.length - 1];
-    solution2ndLastChar = solution[solution.length - 2];
+Array.from(document.getElementsByClassName("function")).forEach(function(element) {
+    element.addEventListener("click", performUpdate);
+})
 
-    functionController();
+document.getElementById("zero").addEventListener("click", performUpdate);
+document.getElementById("period").addEventListener("click", performUpdate);
+document.getElementById("equals").addEventListener("click", performUpdate);
+document.getElementById("clear").addEventListener("click", performUpdate);
 
-    solutionElement.textContent = solution.join(""); // return back to string type
-    answerElement.textContent = answer.join("");
-}
+function performUpdate() {
+    solution = document.getElementById("solution-text-value").textContent;
+    answer = document.getElementById("answer-text-value").textContent;
 
-function functionController() { // classify by class, individual functions classify by id
     switch(event.target.className) {
         case "number":
             writeNumber();
@@ -39,7 +46,7 @@ function functionController() { // classify by class, individual functions class
             writeOperation();
             break;
         case "function":
-            executeFunction();
+            doFunction();
             break;
         case "zero":
             writeZero();
@@ -47,53 +54,54 @@ function functionController() { // classify by class, individual functions class
         case "period":
             writePeriod();
             break;
+        case "equals":
+            doComputation();
+            break;
         default:
-            console.log("switch default");
+            console.log("performUpdate default");
             break;
     }
+
+    document.getElementById("solution-text-value").textContent = solution;
+    document.getElementById("answer-text-value").textContent = answer;
 }
 
-function executeFunction() {
-    switch(event.target.id) {
-        case "clear":
-            solution.pop();
-            break;
-        case "clear-entry":
-            solution.length = 0;
-            answer.length = 0;
-            break;
-        case "plus-minus":
-            solution[0] == "-" ? solution.unshift("-") : solution.shift();
-            break;            
-        default:
-            break;
-    }
-}
-
-function writeNumber() { 
-
-    if(solutionLastChar == "0" || (solutionLastChar == "0" && opExists(solution2ndLastChar))) {
-        solution.pop();
-    }
-
-    solution.push(numbersObj[event.target.id]);
+function writeNumber() {
+    solution += numbersArr[event.target.id];
 }
 
 function writeOperation() {
-    console.log({opArr, solutionLastChar});
-
-    if(!opExists(solutionLastChar)) { // if no operation symbol, write one
-        solution.push(opChars[event.target.id]);
-    }
+    solution += opArr[event.target.id];
 }
 
 function writeZero() {
-    if(solutionLastChar != "0" && !opExists(solution2ndLastChar)) {
-        solution.push("0");
-    }
+    solution += 0;
 }
 
 function writePeriod() {
-
+    solution += ".";
 }
 
+function doFunction() {
+    switch(event.target.id) {
+        case "clear":
+            solution = Array.from(solution);
+            solution.pop();
+            solution = solution.join("");
+            break;
+        case "clear-entry":
+            solution = "";
+            answer = "";
+            break;
+        case "plus-minus":
+            console.log("not implemented yet");
+            break;
+        default:
+            console.log("doFunction default");
+            break;
+    }
+}
+
+function doComputation() { // parsing
+
+}
---
+++ js/scriptOld2.js
@@ -0,0 +1,118 @@
+var solutionElement = document.getElementById("solution-text-value");
+var answerElement = document.getElementById("answer-text-value");
+var solution;
+var answer;
+var solutionLastChar;
+var solution2ndLastChar;
+var numbersObj = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
+var plusSign = String.fromCharCode(43); // plus
+var minusSign = String.fromCharCode(45); // hyphen
+var timesSign = String.fromCharCode(215); // times
+var divideSign = String.fromCharCode(247); // divide
+var opChars = {"plus": String.fromCharCode(43), "minus": String.fromCharCode(45), "times": String.fromCharCode(215), "divide": String.fromCharCode(247)}; // key names matched with element ids for easy reference calls
+var opArr = [opChars["plus"], opChars["minus"], opChars["times"], opChars["divide"]]; // used for checking presence of operation
+var canWriteZero;
+var terms = []
+var digits;
+
+function opExists(myChar) { return opArr.includes(myChar) ? true : false; } // wrote because opArr.includes() is confusing
+
+Array.from(document.getElementsByTagName("button")).forEach(function(element) {
+    element.addEventListener("click", performChanges);
+}); 
+
+function performChanges() {
+    solution = Array.from(solutionElement.textContent); // convert to array for line value manipulation
+    answer = Array.from(answerElement.textContent);
+    solutionLastChar = solution[solution.length - 1];
+    solution2ndLastChar = solution[solution.length - 2];
+
+    functionController();
+
+    solutionElement.textContent = solution.join(""); // return back to string type
+    answerElement.textContent = answer.join("");
+}
+
+function functionController() { // classify by class, individual functions classify by id
+    switch(event.target.className) {
+        case "number":
+            writeNumber();
+            break;
+        case "operation":
+            writeOperation();
+            break;
+        case "function":
+            executeFunction();
+            break;
+        case "zero":
+            writeZero();
+            break;
+        case "period":
+            writePeriod();
+            break;
+        default:
+            console.log("switch default");
+            break;
+    }
+}
+
+function executeFunction() {
+    switch(event.target.id) {
+        case "clear":
+            solution.pop();
+            break;
+        case "clear-entry":
+            solution.length = 0;
+            answer.length = 0;
+            break;
+        case "plus-minus":
+            solution[0] == "-" ? solution.unshift("-") : solution.shift();
+            break;            
+        default:
+            break;
+    }
+}
+
+function writeNumber() { 
+
+    if(solutionLastChar == "0" && opExists(solution2ndLastChar)) {
+        solution.pop();
+    }
+
+    solution.push(numbersObj[event.target.id]);
+}
+
+function writeOperation() {
+    console.log({opArr, solutionLastChar});
+
+    if(!opExists(solutionLastChar)) { // if no operation symbol, write one
+        solution.push(opChars[event.target.id]);
+    }
+}
+
+/*
+writing zero:
+1. when it is a single leading zero
+ - 2nd last char is an operation symbol
+ - 
+2. when it is a zero enclosed by other numbers
+*/
+function writeZero() {
+    // if(solutionLastChar == "0" && opExists(solution2ndLastChar)) {
+    //     canWriteZero = true;
+    // } else {
+    //     canWriteZero = false;
+    // }
+
+    // if(!canWriteZero) {
+    //     solution.push("0")
+    // }
+    solution.push("0");
+}
+
+function writePeriod() {
+    //TODO
+    //FIX WRITEZERO LEADING ZERO ISSUE
+    //MAKE WRITEPERIOD
+}
+
